f68161b4c4c0b6c7939a952c4f8f8f5d64bc8077
Mateusz <lesniak.mat@gmail.com>
2017-12-24 16:38:20 +0100

    Create director in movie form if not present

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,11 +8,18 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @director = Director.find_by(director_params) || Director.new(director_params)
     @movie = Movie.new(movie_params)
+    binding.pry
     if @movie.save
       redirect_to movies_path
     else
       render 'new'
+    end
+    if @director.persisted?
+
+    else
+      @director.save
     end
   end
 
@@ -20,5 +27,9 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :release_date, :duration, :info, :genres, :countries)
+  end
+
+  def director_params
+    params.require(:director).permit(:name, :surname)
   end
 end
