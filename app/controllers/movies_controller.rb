fa1b5db446c27c27ec9ec7537b3d5962965c917e
Mateusz <lesniak.mat@gmail.com>
2017-12-26 12:29:17 +0100

    Add edit movie option

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -36,6 +36,19 @@ class MoviesController < ApplicationController
 
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
