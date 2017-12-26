b84893aa1a9e727e622a6e85d1bd5a10e41779a6
Mateusz <lesniak.mat@gmail.com>
2017-12-26 21:00:41 +0100

    Add not null validation to director and writer models

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,5 +1,7 @@
 class Director < ApplicationRecord
   validates :name, uniqueness: { scope: :surname }
+  validates :name, presence: true
+  validates :surname, presence: true
   has_many :movies_directors
   has_many :directors, through: :movies_directors
 end
--- app/models/writer.rb
+++ app/models/writer.rb
@@ -1,5 +1,7 @@
 class Writer < ApplicationRecord
   validates :name, uniqueness: { scope: :surname }
+  validates :name, presence: true
+  validates :surname, presence: true
   has_many :movies_writers
   has_many :writers, through: :movies_writers
 end
