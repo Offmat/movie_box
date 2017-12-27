86266778266f78e7f4ffa0e0110412cddd622013
Mateusz <lesniak.mat@gmail.com>
2017-12-27 21:57:26 +0100

    Delete movie director relation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
-  resources :movies
+  resources :movies do
+    resources :movies_directors, only: [:destroy]
+  end
   resources :directors, only: [:create]
 end
