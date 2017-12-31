b7acacab9548509c29cae6bed83fb2f04b87842e
Mateusz <lesniak.mat@gmail.com>
2017-12-31 17:52:54 +0100

    Copy directors functionality to writers

---
+++ app/controllers/writers_controller.rb
@@ -0,0 +1,24 @@
+class WritersController < ApplicationController
+  def create
+    @movie = Movie.find(params[:writer][:movie_id])
+    @writer = Writer.find_or_initialize_by(writer_params)
+    if @writer.save
+      MoviesWriter.new(movie: @movie, writer: @writer).save
+      redirect_to edit_movie_path(@movie)
+    else
+      render 'movies/edit'
+    end
+  end
+
+  def destroy
+    @writer = Writer.find(params[:id])
+    MoviesWriter.find_by(movie_id: params[:movie_id], writer: @writer).destroy
+    redirect_to edit_movie_path(params[:movie_id])
+  end
+
+  private
+
+  def writer_params
+    params.require(:writer).permit(:name, :surname)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'movies#index'
   resources :movies
   resources :directors, only: [:create, :destroy]
+  resources :writers, only: [:create, :destroy]
 end
