69553bae5f3c84753b62e2cb2f8ef4b6897d2994
Mateusz <lesniak.mat@gmail.com>
2018-01-03 16:42:20 +0100

    Add not null validation to models

--- app/models/movies_director.rb
+++ app/models/movies_director.rb
@@ -1,5 +1,7 @@
 class MoviesDirector < ApplicationRecord
   validates :movie, uniqueness: { scope: :director }
+  validates :movie, presence: true
+  validates :director, presence: true
   belongs_to :movie
   belongs_to :director
 
--- app/models/movies_writer.rb
+++ app/models/movies_writer.rb
@@ -1,5 +1,7 @@
 class MoviesWriter < ApplicationRecord
   validates :movie, uniqueness: { scope: :writer }
+  validates :movie, presence: true
+  validates :writer, presence: true
   belongs_to :movie
   belongs_to :writer
 
