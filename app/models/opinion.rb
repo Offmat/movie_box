fb9831ad02d7bde6765d2985bbe10a5b5920b9ab
Mateusz <lesniak.mat@gmail.com>
2018-01-08 16:30:08 +0100

    Add partial for overrided error messages

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,6 +1,6 @@
 class Opinion < ApplicationRecord
   validates :score, presence: true, inclusion: 0..10
-  validates :movie, uniqueness: { scope: :user }
+  validates :movie, uniqueness: { scope: :user, message: "You can add only one opinion per movie" }
 
   belongs_to :movie
   belongs_to :user
